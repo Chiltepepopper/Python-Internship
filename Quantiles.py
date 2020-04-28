c335b1c05b75ac79bb401b2cc02ed512c93daa75
J-Baka <55816036+J-Baka@users.noreply.github.com>
2020-04-27 22:53:09 -0700

    Create Quantiles.py

---
+++ Quantiles.py
@@ -0,0 +1,11 @@
+import libpysal
+import geopandas as gpd
+import mapclassify as mc
+import matplotlib.pyplot as plt
+
+columbus = gpd.read_file(libpysal.examples.get_path('columbus.shp'))
+q5 = mc.Quantiles(columbus.CRIME, k=5)
+q5.plot(columbus)
+q5.plot(columbus, axis_on=False)
+q5.plot(columbus, axis_on=False, cmap='Blues')
+plt.show()
