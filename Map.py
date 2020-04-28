5a168164018ec5e6feb5586fe7e885aec42276c9
J-Baka <55816036+J-Baka@users.noreply.github.com>
2020-04-27 22:53:05 -0700

    Update Map.py

--- Map.py
+++ Map.py
@@ -5,13 +5,14 @@ import matplotlib.pyplot as plt
 import geopandas as gpd
 from splot.libpysal import plot_spatial_weights
 
+
 examples.explain('tokyo')
 gdf = gpd.read_file(examples.get_path('tokyomet262.shp'))
 gdf.head()
-weights = Queen.from_dataframe(gdf)
-plot_spatial_weights(weights, gdf)
+weight = Queen.from_dataframe(gdf)
+plot_spatial_weights(weight, gdf)
 plt.show()
-wnp = libpysal.weights.util.nonplanar_neighbors(weights, gdf)
+wnp = libpysal.weights.util.nonplanar_neighbors(weight, gdf)
 plot_spatial_weights(wnp, gdf)
 plt.show()
 plot_spatial_weights(wnp, gdf, nonplanar_edge_kws=dict(color='#4393c3'))
