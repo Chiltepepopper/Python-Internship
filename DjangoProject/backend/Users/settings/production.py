923366f1f5c14c45f9fe285b884e3d9aa2b82c02
Chiltepepopper <60244727+Chiltepepopper@users.noreply.github.com>
2020-04-30 14:09:28 -0700

    Update production.py

--- DjangoProject/backend/Users/settings/production.py
+++ DjangoProject/backend/Users/settings/production.py
@@ -1,3 +1,6 @@
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '5w)ddndhfdvo_0svgibl@k)qbco3x*s=plg3_51@7nf6@3dn(*'
 
