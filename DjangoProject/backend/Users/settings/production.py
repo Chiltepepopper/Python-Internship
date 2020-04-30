465a5705798b99fff550f95d9d34db4830e28148
Chiltepepopper <60244727+Chiltepepopper@users.noreply.github.com>
2020-04-30 14:08:59 -0700

    Create production.py

---
+++ DjangoProject/backend/Users/settings/production.py
@@ -0,0 +1,7 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '5w)ddndhfdvo_0svgibl@k)qbco3x*s=plg3_51@7nf6@3dn(*'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ["backend"]
