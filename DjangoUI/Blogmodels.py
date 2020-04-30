fc587ec05e35f0557829ecb51a2491e547aa49f7
J-Baka <55816036+J-Baka@users.noreply.github.com>
2020-04-30 13:05:40 -0700

    Add files via upload

---
+++ DjangoUI/Blogmodels.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    ip_address = models.GenericIPAddressField(null=True)
+    message = models.TextField()
+
+    def get_absolute_url(self):
+        return reverse('blog-home', kwargs={'pk': self.pk})
---
+++ DjangoUI/Blogviews.py
@@ -0,0 +1,106 @@
+import csv
+import io
+from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from .models import Post
+from .models import Contact
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
+
+
+def react(request):
+    return render(request, 'blog/react.html', {'title': 'React'})
+
+
+@permission_required('admin.can_add_log_entry')
+def contact_upload(request):
+    template = "contact_upload.html"
+    prompt = {
+        'order': 'Order of the CSV should be first_name, last_name, email, ip_address, message'
+    }
+
+    if request.method == "GET":
+        return render(request, template, prompt)
+    csv_file = request.FILES['file']
+
+    if not csv_file.name.endswith('.csv'):
+        messages.error(request, 'This is not a csv file')
+
+    data_set = csv_file.read().decode('UTF-8')
+    io_string = io.StringIO(data_set)
+    next(io_string)
+    for column in csv.reader(io_string, delimiter=',', quotechar="|"):
+        _, created = Contact.objects.update_or_create(
+            first_name=column[0],
+            last_name=column[1],
+            email=column[2],
+            ip_address=column[3],
+            message=column[4]
+        )
+
+    context = {}
+    return render(request, 'blog/contact_upload.html', context)
